23aad2af6ac2227ac4509778753aa30b8076f39d
ManaFujimoto <mana_fujimoto@ricoh-japan.co.jp>
2017-12-13 16:19:24 +0900

    承認・NG画面を新規追加

---
+++ ZEBRA/AdmitDetail.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ZEBRA
+{
+    partial class AdmitDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.bossComments = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.fillDate = new System.Windows.Forms.Label();
+            this.visitDate = new System.Windows.Forms.Label();
+            this.visitTo = new System.Windows.Forms.Label();
+            this.visitType = new System.Windows.Forms.Label();
+            this.content = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HGS行書体", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(193, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "詳細";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(97, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "記入日";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(97, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "訪問日時";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(97, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "訪問先";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(97, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "訪問種別";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(112, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "内容";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "承認",
+            "NG"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 319);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // bossComments
+            // 
+            this.bossComments.Location = new System.Drawing.Point(166, 319);
+            this.bossComments.Multiline = true;
+            this.bossComments.Name = "bossComments";
+            this.bossComments.Size = new System.Drawing.Size(290, 78);
+            this.bossComments.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(262, 299);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 15);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "《上長コメント》";
+            // 
+            // fillDate
+            // 
+            this.fillDate.AutoSize = true;
+            this.fillDate.Location = new System.Drawing.Point(235, 78);
+            this.fillDate.Name = "fillDate";
+            this.fillDate.Size = new System.Drawing.Size(35, 12);
+            this.fillDate.TabIndex = 9;
+            this.fillDate.Text = "label8";
+            // 
+            // visitDate
+            // 
+            this.visitDate.AutoSize = true;
+            this.visitDate.Location = new System.Drawing.Point(235, 121);
+            this.visitDate.Name = "visitDate";
+            this.visitDate.Size = new System.Drawing.Size(35, 12);
+            this.visitDate.TabIndex = 10;
+            this.visitDate.Text = "label9";
+            // 
+            // visitTo
+            // 
+            this.visitTo.AutoSize = true;
+            this.visitTo.Location = new System.Drawing.Point(235, 160);
+            this.visitTo.Name = "visitTo";
+            this.visitTo.Size = new System.Drawing.Size(41, 12);
+            this.visitTo.TabIndex = 11;
+            this.visitTo.Text = "label10";
+            // 
+            // visitType
+            // 
+            this.visitType.AutoSize = true;
+            this.visitType.Location = new System.Drawing.Point(235, 199);
+            this.visitType.Name = "visitType";
+            this.visitType.Size = new System.Drawing.Size(41, 12);
+            this.visitType.TabIndex = 12;
+            this.visitType.Text = "label11";
+            // 
+            // content
+            // 
+            this.content.AutoSize = true;
+            this.content.Location = new System.Drawing.Point(235, 237);
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(41, 12);
+            this.content.TabIndex = 13;
+            this.content.Text = "label12";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("HG行書体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(28, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 34);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "確定";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // AdmitDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(481, 442);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.visitType);
+            this.Controls.Add(this.visitTo);
+            this.Controls.Add(this.visitDate);
+            this.Controls.Add(this.fillDate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.bossComments);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AdmitDetail";
+            this.Text = "AdmitDetail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox bossComments;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label fillDate;
+        private System.Windows.Forms.Label visitDate;
+        private System.Windows.Forms.Label visitTo;
+        private System.Windows.Forms.Label visitType;
+        private System.Windows.Forms.Label content;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ZEBRA/ChangeDetail.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ZEBRA
+{
+    partial class ChangeDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HGS行書体", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(199, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 27);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "詳細";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 31);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "修正";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // ChangeDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(458, 396);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Name = "ChangeDetail";
+            this.Text = "ChangeDetail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
