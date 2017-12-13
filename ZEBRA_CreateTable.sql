38ca4c64abdcacf2da61d2d361f98518e9641f74
HirokazuTakano <hirokazu_takano@ricoh-japan.co.jp>
2017-12-13 17:01:01 +0900

    テーブル作成SQL作成

---
+++ ZEBRA_CreateTable.sql
@@ -0,0 +1,81 @@
+USE zebradb;
+
+--部署テーブルがある場合削除する
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'dbo.TM_SECTION') AND
+   OBJECTPROPERTY(id, N'IsUserTable') = 1)
+      DROP TABLE dbo.TM_SECTION
+
+
+
+-- 部署テーブル作成
+CREATE TABLE dbo.TM_SECTION
+(
+	SECTION_ID varchar(6) NOT NULL,
+	SECTION_NAME varchar(100) NOT NULL,
+ CONSTRAINT PK_TM_SECTION PRIMARY KEY CLUSTERED (SECTION_ID)
+ ) ON [PRIMARY]
+;
+
+
+
+-- 従業員テーブルがある場合削除する
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'dbo.TM_EMPLOYEE') AND
+   OBJECTPROPERTY(id, N'IsUserTable') = 1)
+      DROP TABLE dbo.TM_EMPLOYEE
+
+
+-- 従業員テーブル作成
+CREATE TABLE dbo.TM_EMPLOYEE
+(
+	EMP_ID varchar(8) NOT NULL,
+	EMP_PASSWORD varchar(8) NOT NULL,
+	NAME_SEI varchar(100) NOT NULL,
+	NAME_MEI varchar(100) NOT NULL,
+	BOSS_ID varchar(8) NOT NULL,
+	EMP_SEC_ID varchar(6) NOT NULL,
+	EMP_TEL varchar(15) NOT NULL,
+ CONSTRAINT PK_TM_EMPLOYEE PRIMARY KEY CLUSTERED (EMP_ID)
+ )ON [PRIMARY]
+;
+
+
+-- 顧客テーブルがある場合削除する
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'dbo.TM_CUSTOMER') AND
+   OBJECTPROPERTY(id, N'IsUserTable') = 1)
+      DROP TABLE dbo.TM_CUSTOMER
+
+-- 顧客テーブル作成
+CREATE TABLE dbo.TM_CUSTOMER
+(
+	CUS_ID varchar(8) NOT NULL,
+	COMPANY_NAME varchar(100) NOT NULL,
+	CUS_NAME varchar(100) NOT NULL,
+	CUS_TEL varchar(15) NOT NULL,
+ CONSTRAINT PK_TM_CUSTOMER PRIMARY KEY CLUSTERED (CUS_ID)
+ )ON [PRIMARY]
+;
+
+
+-- 日報テーブルがある場合削除する
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'dbo.TM_DAILY_REPORT') AND
+   OBJECTPROPERTY(id, N'IsUserTable') = 1)
+      DROP TABLE dbo.TM_DAILY_REPORT
+
+
+-- 日報テーブル作成
+CREATE TABLE dbo.TM_DAILY_REPORT(
+	REPORT_ID int IDENTITY(1,1) NOT NULL,
+	CREATE_DATE datetime NOT NULL DEFAULT GETDATE(),
+	UPDATE_DATE datetime NOT NULL DEFAULT GETDATE(),
+	VISIT_STRAT_DATE datetime NOT NULL,
+	VISIT_END_DATE datetime NOT NULL,
+	VISIT_TYPE varchar(50) NOT NULL,
+	DETAILS varchar(max) NOT NULL,
+	CUS_ID varchar(6) NOT NULL,
+	APPROVAL_FLAG int NOT NULL,
+	AUTHOR_ID varchar(8) NOT NULL,
+	AUTHOR_BOSS_ID varchar(8) NOT NULL,
+	BOSS_COMMENT varchar(max) NOT NULL DEFAULT '',
+ CONSTRAINT PK_TM_DAILY_REPORT PRIMARY KEY CLUSTERED ([REPORT_ID])
+) ON [PRIMARY]
+;
