293a4a16a05b8d32b8dc30ef7d64b663ac1ff72a
犬伏　萌々子 <momoko_inubuse@ricoh-japan.co.jp>
2017-12-14 16:57:46 +0900

    日報詳細画面を作成しました

--- ZEBRA/ReportDatail.cs
+++ ZEBRA/ReportDatail.cs
@@ -10,11 +10,28 @@ using System.Windows.Forms;
 
 namespace ZEBRA
 {
+    /// <summary>
+    /// 日報の詳細を表示させます　犬伏
+    /// </summary>
     public partial class ReportDatail : Form
     {
         public ReportDatail()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 詳細へというボタンを押下すると、親から引き継いだ値をラベルに入力する.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReportDatail_Load(object sender, EventArgs e, string fill, string visitDate, string visitTo, string visitType, string content)
+        {
+            this.fillDate.Text = fill;
+            this.visitDate.Text = visitDate;
+            this.visitTo.Text = visitTo;
+            this.visitType.Text = visitType;
+            this.content.Text = content;
         }
     }
 }
