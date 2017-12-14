7976d1a4be4f6a75b619f84ed2db3759c9f51498
ReiIshizuka <rei_ishizuka@ricoh-japan.co.jp>
2017-12-14 15:29:29 +0900

    こもばりできたよ

--- ZEBRA/CommonValidater.cs
+++ ZEBRA/CommonValidater.cs
@@ -8,5 +8,152 @@ namespace ZEBRA
 {
     class CommonValidater
     {
+        private DateTime createDate;
+        private DateTime startDate;
+        private string startHour;
+        private string startMin;
+        private DateTime endDate;
+        private string endHour;
+        private string endMin;
+        private string customerName;
+        private string visitType;
+        private string detail;
+        private DateTime dt = DateTime.Now;
+        private DateTime startDateAll;
+        private DateTime endDateAll;
+
+        int resultDate;
+
+
+
+        public CommonValidater(DateTime createDate, DateTime startDate, string startHour,
+            string startMin, DateTime endDate, string endHour, string endMin, string customerName,
+            string visitType, string detail)
+        {
+            this.createDate = createDate;
+            this.startDate = startDate;
+            this.startHour = startHour;
+            this.startMin = startMin;
+            this.endDate = endDate;
+            this.endHour = endHour;
+            this.endMin = endMin;
+            this.customerName = customerName;
+            this.visitType = visitType;
+            this.detail = detail;
+        }
+
+
+        public Boolean InputCheck()
+        {
+            Boolean result = true;
+
+
+            if (startDate == null || startDate.Equals(""))
+            {
+                result = false;
+            }
+            else if (startHour == null || startHour.Equals(""))
+            {
+                result = false;
+            }
+
+            else if (startMin == null || startMin.Equals(""))
+            {
+                result = false;
+            }
+
+            else if (endDate == null || endDate.Equals(""))
+            {
+                result = false;
+            }
+
+            else if (endHour == null || endHour.Equals(""))
+            {
+                result = false;
+            }
+
+            else if (endMin == null || endMin.Equals(""))
+            {
+                result = false;
+            }
+            else
+            {
+
+                //日にち、時間、分をつなげてDateTime型をstartDateAllへ
+                string str = startDate.ToString() + "" + startHour + ":" + startMin + ":" + "00";
+
+                startDateAll = DateTime.Parse(str);
+
+                //日にち、時間、分をつなげてDateTime型をendDateAllへ
+                string str2 = endDate.ToString() + "" + endHour + ":" + endMin + ":" + "00";
+
+                endDateAll = DateTime.Parse(str2);
+
+
+                //開始日時が未来にならないか
+                resultDate = DateTime.Compare(startDateAll, dt);
+
+                if (resultDate == 1)
+                {
+                    result = false;
+                }
+                else
+                {
+                    //終了が開始より前にならないか
+                    resultDate = DateTime.Compare(endDateAll, startDateAll);
+                    if (resultDate == 1)
+                    {
+                        result = false;
+
+                    }
+
+                }
+
+
+            }
+
+
+            if (customerName == null || customerName.Equals(""))
+            {
+                result = false;
+            }
+
+            if (visitType == null || visitType.Equals(""))
+            {
+                result = false;
+            }
+
+
+
+            if (detail == null || detail.Equals(""))
+            {
+                result = false;
+            }
+
+
+
+
+            return result;
+
+
+
+        }
+
+        public DateTime StartDateAll
+        {
+            get
+            {
+                return startDateAll;
+            }
+        }
+
+        public DateTime EndDateAll
+        {
+            get
+            {
+                return endDateAll;
+            }
+        }
     }
 }
+
