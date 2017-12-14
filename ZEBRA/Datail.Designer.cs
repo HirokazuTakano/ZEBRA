7d9b0b42852b4ef13805d1e9bc8d7658f42d2bf0
ManaFujimoto <mana_fujimoto@ricoh-japan.co.jp>
2017-12-14 09:12:42 +0900

    日報詳細画面を追加

--- ZEBRA/Datail.Designer.cs
+++ ZEBRA/Datail.Designer.cs
@@ -28,11 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.content = new System.Windows.Forms.Label();
+            this.visitType = new System.Windows.Forms.Label();
+            this.visitTo = new System.Windows.Forms.Label();
+            this.visitDate = new System.Windows.Forms.Label();
+            this.fillDate = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // content
+            // 
+            this.content.AutoSize = true;
+            this.content.Location = new System.Drawing.Point(273, 289);
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(41, 12);
+            this.content.TabIndex = 24;
+            this.content.Text = "label12";
+            // 
+            // visitType
+            // 
+            this.visitType.AutoSize = true;
+            this.visitType.Location = new System.Drawing.Point(273, 251);
+            this.visitType.Name = "visitType";
+            this.visitType.Size = new System.Drawing.Size(41, 12);
+            this.visitType.TabIndex = 23;
+            this.visitType.Text = "label11";
+            // 
+            // visitTo
+            // 
+            this.visitTo.AutoSize = true;
+            this.visitTo.Location = new System.Drawing.Point(273, 212);
+            this.visitTo.Name = "visitTo";
+            this.visitTo.Size = new System.Drawing.Size(41, 12);
+            this.visitTo.TabIndex = 22;
+            this.visitTo.Text = "label10";
+            // 
+            // visitDate
+            // 
+            this.visitDate.AutoSize = true;
+            this.visitDate.Location = new System.Drawing.Point(273, 173);
+            this.visitDate.Name = "visitDate";
+            this.visitDate.Size = new System.Drawing.Size(35, 12);
+            this.visitDate.TabIndex = 21;
+            this.visitDate.Text = "label9";
+            // 
+            // fillDate
+            // 
+            this.fillDate.AutoSize = true;
+            this.fillDate.Location = new System.Drawing.Point(273, 130);
+            this.fillDate.Name = "fillDate";
+            this.fillDate.Size = new System.Drawing.Size(35, 12);
+            this.fillDate.TabIndex = 20;
+            this.fillDate.Text = "label8";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(150, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 15);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "内容";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(135, 251);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "訪問種別";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(135, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "訪問先";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(135, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "訪問日時";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("HGP行書体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(135, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "記入日";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HGS行書体", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(214, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 27);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "詳細";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(33, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 27);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "トップメニュー";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Datail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(481, 358);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.visitType);
+            this.Controls.Add(this.visitTo);
+            this.Controls.Add(this.visitDate);
+            this.Controls.Add(this.fillDate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Datail";
             this.Text = "Datail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label content;
+        private System.Windows.Forms.Label visitType;
+        private System.Windows.Forms.Label visitTo;
+        private System.Windows.Forms.Label visitDate;
+        private System.Windows.Forms.Label fillDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
