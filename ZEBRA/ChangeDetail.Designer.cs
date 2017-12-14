a31d254bd4a6e7da81527f7ca3c1e2e184d14c19
ManaFujimoto <mana_fujimoto@ricoh-japan.co.jp>
2017-12-14 09:23:01 +0900

    日報修正画面を作成

--- ZEBRA/ChangeDetail.Designer.cs
+++ ZEBRA/ChangeDetail.Designer.cs
@@ -29,35 +29,256 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.another = new System.Windows.Forms.RadioButton();
+            this.their = new System.Windows.Forms.RadioButton();
+            this.opponentHouse = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.customer_search = new System.Windows.Forms.Button();
+            this.customer = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.register = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mail = new System.Windows.Forms.RadioButton();
+            this.tell = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("HGS行書体", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(199, 28);
+            this.label1.Location = new System.Drawing.Point(266, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 27);
+            this.label1.Size = new System.Drawing.Size(124, 27);
             this.label1.TabIndex = 14;
-            this.label1.Text = "詳細";
+            this.label1.Text = "日報修正";
             // 
-            // button2
+            // dateTimePicker3
             // 
-            this.button2.Location = new System.Drawing.Point(342, 337);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(71, 31);
-            this.button2.TabIndex = 16;
-            this.button2.Text = "修正";
-            this.button2.UseVisualStyleBackColor = true;
+            this.dateTimePicker3.Location = new System.Drawing.Point(412, 108);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(200, 19);
+            this.dateTimePicker3.TabIndex = 42;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(156, 108);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 19);
+            this.dateTimePicker2.TabIndex = 41;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(377, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 40;
+            this.label8.Text = "～";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(45, 201);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "訪問種別";
+            // 
+            // another
+            // 
+            this.another.AutoSize = true;
+            this.another.Location = new System.Drawing.Point(268, 219);
+            this.another.Name = "another";
+            this.another.Size = new System.Drawing.Size(54, 16);
+            this.another.TabIndex = 38;
+            this.another.TabStop = true;
+            this.another.Text = "その他";
+            this.another.UseVisualStyleBackColor = true;
+            // 
+            // their
+            // 
+            this.their.AutoSize = true;
+            this.their.Location = new System.Drawing.Point(156, 219);
+            this.their.Name = "their";
+            this.their.Size = new System.Drawing.Size(47, 16);
+            this.their.TabIndex = 37;
+            this.their.TabStop = true;
+            this.their.Text = "自社";
+            this.their.UseVisualStyleBackColor = true;
+            // 
+            // opponentHouse
+            // 
+            this.opponentHouse.AutoSize = true;
+            this.opponentHouse.Location = new System.Drawing.Point(366, 197);
+            this.opponentHouse.Name = "opponentHouse";
+            this.opponentHouse.Size = new System.Drawing.Size(59, 16);
+            this.opponentHouse.TabIndex = 36;
+            this.opponentHouse.TabStop = true;
+            this.opponentHouse.Text = "相手宅";
+            this.opponentHouse.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(154, 371);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 12);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "高野　浩一";
+            // 
+            // customer_search
+            // 
+            this.customer_search.Location = new System.Drawing.Point(326, 156);
+            this.customer_search.Name = "customer_search";
+            this.customer_search.Size = new System.Drawing.Size(57, 23);
+            this.customer_search.TabIndex = 34;
+            this.customer_search.Text = "検索";
+            this.customer_search.UseVisualStyleBackColor = true;
+            // 
+            // customer
+            // 
+            this.customer.Location = new System.Drawing.Point(156, 158);
+            this.customer.Name = "customer";
+            this.customer.Size = new System.Drawing.Size(155, 19);
+            this.customer.TabIndex = 33;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 253);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(438, 100);
+            this.textBox1.TabIndex = 32;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(156, 78);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 19);
+            this.dateTimePicker1.TabIndex = 31;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "トップページ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // register
+            // 
+            this.register.Location = new System.Drawing.Point(33, 402);
+            this.register.Name = "register";
+            this.register.Size = new System.Drawing.Size(75, 23);
+            this.register.TabIndex = 29;
+            this.register.Text = "修正";
+            this.register.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 371);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "上司";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "内容";
+            // 
+            // mail
+            // 
+            this.mail.AutoSize = true;
+            this.mail.Location = new System.Drawing.Point(268, 197);
+            this.mail.Name = "mail";
+            this.mail.Size = new System.Drawing.Size(51, 16);
+            this.mail.TabIndex = 26;
+            this.mail.TabStop = true;
+            this.mail.Text = "メール";
+            this.mail.UseVisualStyleBackColor = true;
+            // 
+            // tell
+            // 
+            this.tell.AutoSize = true;
+            this.tell.Location = new System.Drawing.Point(156, 197);
+            this.tell.Name = "tell";
+            this.tell.Size = new System.Drawing.Size(47, 16);
+            this.tell.TabIndex = 25;
+            this.tell.TabStop = true;
+            this.tell.Text = "電話";
+            this.tell.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 12);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "訪問先の顧客";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "日時";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(55, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 12);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "記入日";
             // 
             // ChangeDetail
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Ivory;
-            this.ClientSize = new System.Drawing.Size(458, 396);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(665, 437);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.another);
+            this.Controls.Add(this.their);
+            this.Controls.Add(this.opponentHouse);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.customer_search);
+            this.Controls.Add(this.customer);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.register);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mail);
+            this.Controls.Add(this.tell);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label1);
             this.Name = "ChangeDetail";
             this.Text = "ChangeDetail";
@@ -69,6 +290,26 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton another;
+        private System.Windows.Forms.RadioButton their;
+        private System.Windows.Forms.RadioButton opponentHouse;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button customer_search;
+        private System.Windows.Forms.TextBox customer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button register;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton mail;
+        private System.Windows.Forms.RadioButton tell;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label9;
     }
 }
