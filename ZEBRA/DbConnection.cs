0ff179e0dc20284ab22c6fb6400331de45ecf938
犬伏　萌々子 <momoko_inubuse@ricoh-japan.co.jp>
2017-12-14 14:32:26 +0900

    DBConnection作成しました

--- ZEBRA/DbConnection.cs
+++ ZEBRA/DbConnection.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ZEBRA
 {
-    class DbConnection
+    /// <summary>
+    /// データベースのopen/closeするクラスです.
+    /// </summary>
+    public class DbConnection
     {
+        SqlConnection con = new SqlConnection();
+
+        /// <summary>
+        /// データベースに接続するクラスです（必ずクローズしてください！）.
+        /// </summary>
+        public void dbConnect()
+        {
+            
+            // DBへの接続文字列。
+            con.ConnectionString = "data source=localhost\\SQLEXPRESS;" + // 接続先のDBサーバーを指定
+                                   "initial catalog=testdb;" +            // 接続先のDBを指定
+                                   "user id=sa;" +                        // ユーザー
+                                   "password=p@ssw0rd;" +                 // パスワード
+                                   "Connect Timeout=60;";
+            try
+            {
+                con.Open();
+
+            } catch(SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// データベースをクローズします。
+        /// </summary>
+        public void dbClose()
+        {
+            con.Close();
+        }
+
     }
 }
